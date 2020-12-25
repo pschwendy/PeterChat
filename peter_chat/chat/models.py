d82d8440eb2a7bf1e3e280cec1f962c4f51ad9fa
pschwendy <peterschwende@gmail.com>
2020-12-25 13:35:16 -0500

    edited models

--- peter_chat/chat/models.py
+++ peter_chat/chat/models.py
@@ -7,9 +7,27 @@ class User(models.Model):
     first_name = models.CharField(max_length=32)
     last_name = models.CharField(max_length=40)
     on_delete=models.CASCADE
+    class Status(models.TextChoices):
+        ACTIVE = 'A', _('Active')
+        IDLE = 'I', _('Idle')
+        OFFLINE = 'O', _('Offline')
+    status = models.CharField(
+        max_length=1,
+        choices=Status.choices,
+        default=Status.ACTIVE,
+    )
     profile_picture = models.ImageField(upload_to='profile_pictures/')
     chats = models.ManyToManyField(UserChat)
     friends = models.ManyToManyField(User)
+
+    def __status__(self):
+        return self.Status.label
+
+    def __username__(self):
+        return self.username
+
+    def __name__(self):
+        return self.first_name + " " + self.last_name
 
 # Model for a user reference to a chat
 class UserChat(models.Model):
@@ -19,9 +37,12 @@ class UserChat(models.Model):
 
 # Chat model
 class Chat(models.Model):
-    users = models.ManyToManyField(User)
+    participates = models.ManyToManyField(User, related_name='chats')
     messages = models.ManyToManyField(Message, blank=True))
     private = models.BooleanField(default=True)
+
+    def __str__(self):
+        return "{}".format(self.pk)
 
 # Message model
 class Message(models.Model):
@@ -29,5 +50,3 @@ class Message(models.Model):
     image = models.ImageField(upload_to='message_images/', blank=True)
     sender = models.IntField()
     timestamp = models.DateTimeField(auto_now_add=True)
-
-# Create your models here.
